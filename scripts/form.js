aa6d91949ebfb30a77bc2db3c4f6d8e6d78630dc
yahaya2006 <diaminata2006@gmail.com>
2026-06-03 13:27:25 +0100

    Add product review form and review submission functionality; implement dynamic product selection and review counter

---
+++ scripts/form.js
@@ -0,0 +1,45 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').innerHTML = currentYear;
+
+document.getElementById('lastModified').innerHTML = document.lastModified
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+const select = document.querySelector('#productName');
+products.forEach(product => {
+    let option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
+})
+
+let reviewCount = Number(localStorage.getItem('reviewCount')) || 0;
+reviewCount++;
+localStorage.setItem('reviewCount', reviewCount);
+document.getElementById('counter').textContent = `Total reviews submitted: ${reviewCount}`;
+
