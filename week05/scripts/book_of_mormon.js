585dbfd45f4d8c101b401494f31d7c1e5e323715
yahaya2006 <diaminata2006@gmail.com>
2026-06-03 10:38:26 +0100

    Add Book of Mormon page with input functionality and styles; implement chapter list management

---
+++ week05/scripts/book_of_mormon.js
@@ -0,0 +1,45 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#addBtn');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+chaptersArray.forEach(chapter => { displayList(chapter) });
+
+button.addEventListener('click', () => {
+  if (input.value != '') {
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
+    input.focus();
+  }
+  });
+
+function displayList(item) {
+  let li = document.createElement('li');
+  let deletebutton = document.createElement('button');
+  li.textContent = item;
+  deletebutton.textContent = 'X';
+  deletebutton.classList.add('delete');
+  li.append(deletebutton);
+  list.append(li);
+  deletebutton.addEventListener('click', () => {
+    list.removeChild(li);
+    deleteChapter(li.textContent);
+    input.focus();
+  });
+}
+
+function setChapterList() {
+  localStorage.setItem('chapters', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('chapters'));
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter((item) => item != chapter);
+  setChapterList();
+}
